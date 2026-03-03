9b1039f89381e838db3feed6692a4fb2b2f6224b
Rebecca Dankowich <r.dankowich@gmail.com>
2026-03-02 19:32:32 -0500

    Added paraglider model

--- Assets/Scripts/Camera/ThirdPersonCam.cs
+++ Assets/Scripts/Camera/ThirdPersonCam.cs
@@ -1,38 +1,30 @@
 using UnityEngine;
 
-public class ThirdPersonCam : MonoBehaviour
+public class ThirdPersonCam: MonoBehaviour
 {
     [Header("References")]
     public Transform orientation;
     public Transform player;
     public Transform playerObj;
     public Rigidbody rb;
-
     public float rotationSpeed;
-
     private PlayerInputHandler input;
-
-    private void Start()
+private void Start()
+{
+    Cursor.lockState = CursorLockMode.Locked;
+    Cursor.visible = false;
+    input = player.GetComponent<PlayerInputHandler>();
+}
+private void Update()
+{
+    // Rotate orientation
+    Vector3 viewDirection = player.position - new Vector3(transform.position.x, player.position.y, transform.position.z);
+    orientation.forward = viewDirection.normalized;
+    // rotate the player object
+    Vector3 inputDirection = orientation.forward * input.MoveInput.y + orientation.right * input.MoveInput.x;
+    if (inputDirection != Vector3.zero)
     {
-        Cursor.lockState = CursorLockMode.Locked;
-        Cursor.visible = false;
-
-        input = player.GetComponent<PlayerInputHandler>();
+        playerObj.forward = Vector3.Slerp(playerObj.forward, inputDirection.normalized, Time.deltaTime * rotationSpeed);
     }
-
-    private void Update()
-    {
-        // Rotate orientation
-        Vector3 viewDirection = player.position - new Vector3(transform.position.x, player.position.y, transform.position.z);
-        orientation.forward = viewDirection.normalized;
-
-        // rotate the player object
-        Vector3 inputDirection = orientation.forward * input.MoveInput.y + orientation.right * input.MoveInput.x;
-
-        if (inputDirection != Vector3.zero)
-        {
-            playerObj.forward = Vector3.Slerp(playerObj.forward, inputDirection.normalized, Time.deltaTime * rotationSpeed);
-        }
-
-    }
+}
 }
